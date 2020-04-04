96eb5bd248be88fbdfaad338eb93accbf5b6aecd
M4R14-miriadaX <jrpv1951@hotmail.com>
2020-04-04 20:25:52 +0200

    proyecto simple

---
+++ src/main/java/com/maria/proyecto/App.java
@@ -0,0 +1,13 @@
+package com.maria.proyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Util.mostrarMensaje( "Hola mundo!" );
+    }
+}
---
+++ src/main/java/com/maria/proyecto/Util.java
@@ -0,0 +1,10 @@
+package com.maria.proyecto;
+
+
+public class Util 
+{
+    public static void mostrarMensaje( String mensaje )
+    {
+        System.out.println( mensaje );
+    }
+}
